2b69866761becc4fdc9fc76c1cedaafd91f2af73
talespin <newtale@naver.com>
2024-01-21 21:09:27 +0900

    shell 추

---
+++ src/worknet_crawler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python worknet_crawler.py -l ../list/worknet_1json
+python worknet_crawler.py -l ../list/worknet_2.json
+python worknet_crawler.py -l ../list/worknet_3.json
+python worknet_crawler.py -l ../list/worknet_4.json
+python worknet_crawler.py -l ../list/worknet_5.json
+python worknet_crawler.py -l ../list/worknet_6.json
+python worknet_crawler.py -l ../list/worknet_7.json
+python worknet_crawler.py -l ../list/worknet_8.json
+python worknet_crawler.py -l ../list/worknet_9.json
+python worknet_crawler.py -l ../list/worknet_10.json
+
