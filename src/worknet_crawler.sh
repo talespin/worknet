5ea0d5bfcbcd680c7f645e37252c49308da3b26c
Choi Jonghwan <newtale@naver.com>
2024-01-23 14:31:07 +0900

    Update worknet_crawler.sh

--- src/worknet_crawler.sh
+++ src/worknet_crawler.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python worknet_crawler.py -l ../list/worknet_1json
-python worknet_crawler.py -l ../list/worknet_2.json
-python worknet_crawler.py -l ../list/worknet_3.json
-python worknet_crawler.py -l ../list/worknet_4.json
-python worknet_crawler.py -l ../list/worknet_5.json
-python worknet_crawler.py -l ../list/worknet_6.json
-python worknet_crawler.py -l ../list/worknet_7.json
-python worknet_crawler.py -l ../list/worknet_8.json
-python worknet_crawler.py -l ../list/worknet_9.json
-python worknet_crawler.py -l ../list/worknet_10.json
+python 3.worknet_crawler.py -l ../list/worknet_1.json
+python 3.worknet_crawler.py -l ../list/worknet_2.json
+python 3.worknet_crawler.py -l ../list/worknet_3.json
+python 3.worknet_crawler.py -l ../list/worknet_4.json
+python 3.worknet_crawler.py -l ../list/worknet_5.json
+python 3.worknet_crawler.py -l ../list/worknet_6.json
+python 3.worknet_crawler.py -l ../list/worknet_7.json
+python 3.worknet_crawler.py -l ../list/worknet_8.json
+python 3.worknet_crawler.py -l ../list/worknet_9.json
+python 3.worknet_crawler.py -l ../list/worknet_10.json
 
