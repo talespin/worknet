b3b7c81a0b78dc23d9c5624afeb760d885f8427f
Choi Jonghwan <newtale@naver.com>
2024-02-15 16:43:58 +0900

    Update worknet_crawler.sh

--- src/worknet_crawler.sh
+++ src/worknet_crawler.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-python 3.worknet_crawler.py -l ../list/worknet_1.json
-python 3.worknet_crawler.py -l ../list/worknet_2.json
-python 3.worknet_crawler.py -l ../list/worknet_3.json
-python 3.worknet_crawler.py -l ../list/worknet_4.json
-python 3.worknet_crawler.py -l ../list/worknet_5.json
-python 3.worknet_crawler.py -l ../list/worknet_6.json
-python 3.worknet_crawler.py -l ../list/worknet_7.json
-python 3.worknet_crawler.py -l ../list/worknet_8.json
-python 3.worknet_crawler.py -l ../list/worknet_9.json
-python 3.worknet_crawler.py -l ../list/worknet_10.json
+cd /mnt/work/worknet/src
 
+while :
+do
+	python 1.worknet_list.py -y=yes;python 3.worknet_crawler_master.py
+        sleep 1h
+done
