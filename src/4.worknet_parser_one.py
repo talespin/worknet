f1051e3fa4ab516ac1068a9fa586b27fab795018
Choi Jonghwan <newtale@naver.com>
2024-02-16 17:15:22 +0900

    Update 4.worknet_parser_one.py

--- src/4.worknet_parser_one.py
+++ src/4.worknet_parser_one.py
@@ -26,38 +26,41 @@ from multiprocessing import Pool
 
 
 def parser(id:str) -> None:
-    file_name = f'../crawl/{id}/{id}.html'
-    logging.info(f' parse : {file_name}')
-    if not os.path.exists(file_name): return
-    json_file = f'../result/{id}.json'
-    if os.path.exists(json_file): return
-    doc = None
-    result = dict(id=id)
-    with open(file_name, 'rt', encoding='utf-8') as fs:
-        doc = bs(fs.read(), 'html.parser')
-    job_title = doc.find('p', {'class':'tit'}).text.strip()
-    result.update(dict(title=job_title))
-    _summarys = doc.find('div', {'class':'info'}).find_all('li')
-    dct_summary = {}
-    for _summary in _summarys:
-        for dt, dd in zip(_summary.find('strong'), _summary.find('span')):
-            dct_summary.update({'summary:'+dt.text.strip():dd.text.strip()})
-    result.update(dct_summary)
-    _jv_conts = doc.find('div', {'class':'right'}).find('div', {'class':'info'}).find_all('li')
-    dct_company = {}
-    for _jv_cont in _jv_conts:
-        for dt, dd in zip(_jv_cont.find('strong'), _jv_cont.find('div')):
-            dct_company.update({'company:'+dt.text.strip():dd.text.strip()})
-    result.update(dct_company)
-    result.update({doc.find('div', {'class':'careers-table'}).find('thead').text.strip(): doc.find('div', {'class':'careers-table'}).find('tbody').text.replace('\xa0','').strip()})
-    _dct_careers = {}  
-    ths = doc.find('div', {'class':'careers-table v1 center mt20'}).find('thead').find_all('th')
-    tds = doc.find('div', {'class':'careers-table v1 center mt20'}).find('tbody').find_all('td')
-    for th, td in zip(ths, tds):
-        _dct_careers.update({th.text.replace("\xa0","").strip():td.text.replace("\xa0","").strip()})
-    result.update(_dct_careers)        
-    with open(json_file, 'wt') as fs:
-        _ = fs.write(json.dumps(result).decode('utf-8'))
+    try:
+        file_name = f'../crawl/{id}/{id}.html'
+        logging.info(f' parse : {file_name}')
+        if not os.path.exists(file_name): return
+        json_file = f'../result/{id}.json'
+        if os.path.exists(json_file): return
+        doc = None
+        result = dict(id=id)
+        with open(file_name, 'rt', encoding='utf-8') as fs:
+            doc = bs(fs.read(), 'html.parser')
+        job_title = doc.find('p', {'class':'tit'}).text.strip()
+        result.update(dict(title=job_title))
+        _summarys = doc.find('div', {'class':'info'}).find_all('li')
+        dct_summary = {}
+        for _summary in _summarys:
+            for dt, dd in zip(_summary.find('strong'), _summary.find('span')):
+                dct_summary.update({'summary:'+dt.text.strip():dd.text.strip()})
+        result.update(dct_summary)
+        _jv_conts = doc.find('div', {'class':'right'}).find('div', {'class':'info'}).find_all('li')
+        dct_company = {}
+        for _jv_cont in _jv_conts:
+            for dt, dd in zip(_jv_cont.find('strong'), _jv_cont.find('div')):
+                dct_company.update({'company:'+dt.text.strip():dd.text.strip()})
+        result.update(dct_company)
+        result.update({doc.find('div', {'class':'careers-table'}).find('thead').text.strip(): doc.find('div', {'class':'careers-table'}).find('tbody').text.replace('\xa0','').strip()})
+        _dct_careers = {}  
+        ths = doc.find('div', {'class':'careers-table v1 center mt20'}).find('thead').find_all('th')
+        tds = doc.find('div', {'class':'careers-table v1 center mt20'}).find('tbody').find_all('td')
+        for th, td in zip(ths, tds):
+            _dct_careers.update({th.text.replace("\xa0","").strip():td.text.replace("\xa0","").strip()})
+        result.update(_dct_careers)        
+        with open(json_file, 'wt') as fs:
+            _ = fs.write(json.dumps(result).decode('utf-8'))
+    except Exception as e:
+        logging.error(str(e))	
 
 
 def main():
