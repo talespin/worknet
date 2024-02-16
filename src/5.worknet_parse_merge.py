4f8eb5c185c88426d0676c8b5ec113cdac82ea72
Choi Jonghwan <newtale@naver.com>
2024-02-16 16:53:01 +0900

    Create 5.worknet_parse_merge.py

---
+++ src/5.worknet_parse_merge.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import orjson as json
+from glob import glob
+
+
+result = []
+for file_name in glob('../result/*.json'):
+    try:
+        with open(file_name, 'rt', encoding='utf-8') as fs:
+            result.append(json.loads(fs.read()))
+    except:
+        print(f'parse error:{file_name}')
+
+df = pd.DataFrame(result)
+df.to_csv('../result/worknet.csv', encoding='cp949')
+df.to_excel('../result/worknet.xlsx')
