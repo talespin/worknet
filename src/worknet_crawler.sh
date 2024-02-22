4246ffc392ef7a7b8640b47836df99d56d7aa5e6
talespin <newtale@naver.com>
2024-02-22 13:30:08 +0900

    shell 변경

--- src/worknet_crawler.sh
+++ src/worknet_crawler.sh
@@ -5,7 +5,7 @@ cd /mnt/work/worknet/src
 while :
 do
 	python -u 1.worknet_list.py -y=yes
-	python -u 2.worknet_crawler_master.py
+	python -u 3.worknet_crawler_master.py
         echo `date`
 	echo "1시간 쉬었다가 다시 시작합니다."
         sleep 1h
